b33c382d0dc39120347dd06eec07cc0b298e9348
ShirleenChebet <shirlynshirley9@gmail.com>
2024-10-08 09:14:51 +0300

    fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID").value;
+  
+      fetch(`http://localhost:3000/movies/${input}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        })
+        .catch((error) => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
+  
